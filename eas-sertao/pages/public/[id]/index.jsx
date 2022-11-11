d0ba2de5a6387c64539f3dc52b2164b7e5f7026f
GABRIEL COSTA PEREIRA <103195@campus.sertao.ifrs.edu.br>
2022-11-11 10:06:51 -0300

    banner

--- eas-sertao/pages/public/[id]/index.jsx
+++ eas-sertao/pages/public/[id]/index.jsx
@@ -1,12 +1,13 @@
-import { Header } from '../../../src/components/Header/Header'
-import { Footer } from '../../../src/components/Footer/Footer'
+import { Header } from '../../../src/components/Header/Header';
+import { Footer } from '../../../src/components/Footer/Footer';
 import { useRouter } from "next/router";
 import conectarDB from "../../../lib/dbConnect";
 import Empresa from "../../../models/Empresa";
 import Link from 'next/link';
-import s from '../../../styles/container.module.scss'
-import Image from 'next/image'
-import comercio1 from '../../../public/static/agroveterinaria.jpg'
+import s from '../../../styles/container.module.scss';
+import Image from 'next/image';
+import comercio1 from '../../../public/static/agroveterinaria.jpg';
+import Banner from '../../../src/components/Banner/Banner';
 
 const EmpresaPage = ({ success, error, empresa }) => {
     const router = useRouter();
@@ -23,29 +24,19 @@ const EmpresaPage = ({ success, error, empresa }) => {
         );
     }
 
-
     return (
         <div>
             <header>
                 <Header />
             </header>
             <div className={s.container}>
-                <h2>Informações</h2>
+
                 <div className={s.contentEmpresa}>
                     <div className="card-body">
-                        <div>
-                            <Image
-                                src={comercio1}
-                                alt="Picture do comercio"
-                            />
-                        </div>
-
                         <p>
-                            -Nome Fantasia: {empresa.namefantasia}
+                            {empresa.namefantasia}
                         </p>
-                        <p>
-                            -Endereço: {empresa.endereco}
-                        </p>
+                        <Banner></Banner>
                         <p>
                             -Telefone/Whatsapp: {empresa.telefonecelular}
                         </p>
@@ -61,6 +52,14 @@ const EmpresaPage = ({ success, error, empresa }) => {
                         <p>
                             -Responsável: {empresa.responsavel}
                         </p>
+                        <p>
+                            -Endereço: {empresa.endereco}
+                        </p>
+                        <div>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14085.332636938774!2d-52.2656117!3d-28.0448504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94e2e5e264da7f39%3A0xd7748730de499f58!2sIFRS%20-%20Campus%20Sert%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1668169807609!5m2!1spt-BR!2sbr"
+                                width="80%" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                        
                         <Link href="/">
                             <a className="btn btn-dark btn-sm me-2">Voltar</a>
                         </Link>
