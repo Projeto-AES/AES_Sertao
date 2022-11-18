9e7a921fcf96e5faa7d8fb2f6ebc41038934ae91
gabrieltcholoko <gabrielcppereira@gmail.com>
2022-11-17 23:55:39 -0300

    ajuste tela empresa

--- eas-sertao/pages/public/[id]/index.jsx
+++ eas-sertao/pages/public/[id]/index.jsx
@@ -34,30 +34,32 @@ const EmpresaPage = ({ success, error, empresa }) => {
                         <p>
                             {empresa.namefantasia}
                         </p>
+
                         <Banner></Banner>
-                        <p>
-                            -Telefone/Whatsapp: {empresa.telefonecelular}
+                        <p></p>
+                        <p className={s.conteudo}>
+                            Telefone/Whatsapp: {empresa.telefonecelular}
                         </p>
-                        <p>
-                            -CNPJ: {empresa.cnpj}
+                        <p className={s.conteudo}>
+                            CNPJ: {empresa.cnpj}
                         </p>
-                        <p>
-                            -Razão social: {empresa.namejuridico}
+                        <p className={s.conteudo}>
+                            Razão social: {empresa.namejuridico}
                         </p>
-                        <p>
-                            -E-mail para contato: {empresa.email}
+                        <p className={s.conteudo}>
+                            E-mail para contato: {empresa.email}
                         </p>
-                        <p>
-                            -Responsável: {empresa.responsavel}
+                        <p className={s.conteudo}>
+                            Responsável: {empresa.responsavel}
                         </p>
-                        <p>
-                            -Endereço: {empresa.endereco}
+                        <p className={s.conteudo}>
+                            Endereço: {empresa.endereco}
                         </p>
                         <div>
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14085.332636938774!2d-52.2656117!3d-28.0448504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94e2e5e264da7f39%3A0xd7748730de499f58!2sIFRS%20-%20Campus%20Sert%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1668169807609!5m2!1spt-BR!2sbr"
                                 width="80%" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
-                        
+
                         <Link href="/">
                             <a className="btn btn-dark btn-sm me-2">Voltar</a>
                         </Link>
